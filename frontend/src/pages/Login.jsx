0d91070b0eebaf017607c2c88636d83ad531c72a
Simon Tam <simontam19@outlook.com>
2024-04-18 16:59:47 +0100

    Create register and login page and allow new user to register

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+
+function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <form className="login" onSubmit={handleSubmit}>
+      <h3>Login</h3>
+      <label htmlFor="email">Email:</label>
+      <input 
+        type="email" 
+        onChange={(e) => {setEmail(e.target.value)}} 
+        value={email} 
+      />
+
+    <label htmlFor="password">Password:</label>
+      <input 
+        type="password" 
+        onChange={(e) => {setPassword(e.target.value)}} 
+        value={password} 
+      />
+
+      <button>Login</button>
+    </form>
+  )
+}
+
+export default Login
